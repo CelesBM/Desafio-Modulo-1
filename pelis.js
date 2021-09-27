95ea20a6d7731f9f70c97e2b3230bf6e862071cb
CelesBM <90277599+CelesBM@users.noreply.github.com>
2021-09-27 11:15:43 -0300

    Update pelis.js

--- pelis.js
+++ pelis.js
@@ -41,3 +41,31 @@ const sortBy = function (propiedad, arrayDePelis) {
   });
     return sort;
 }
+
+exports.searchByCriteria = function (criterios) {
+            // comienzo un array vacio que voy a empezar a rellenar con las respuestas de las funciones
+    let tmp = getAll();
+          
+    if (criterios.search) {
+        console.log("hay search y es", criterios.search);
+        tmp = searchByTitle(criterios.searchTitle, tmp);
+    } else {
+        console.log("no hay search");
+            }
+           
+    if (criterios.search) {
+        console.log("hay search y es", criterios.search);
+        tmp = searchByTag(criterios.searchTag, tmp);
+    } else {
+        console.log("no hay search");
+            }
+          
+    if (criterios.sort) {
+        tmp = sortBy(criterios.sort, tmp);
+        console.log("hay sort y es", criterios.sort);
+    } else {
+        console.log("no hay sort");
+ }
+            // .. y asi
+            return tmp;
+          };
