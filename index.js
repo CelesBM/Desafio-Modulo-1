f9e4d99bdd11c4036b31d51ad46b92f2f43cafcf
CelesBM <90277599+CelesBM@users.noreply.github.com>
2021-09-27 12:13:03 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,22 @@
 const pelis = require("./pelis");
 
-function parsearARGV() {
-
-  return {
-    search: "drama",
-    sort: "rating",
-  };
+function parsearARGV(argv) {
+  
+  const respuesta = {};
+  argv.forEach(function (item, ind) {
+    if (item.startsWith("--")) {
+      const propiedadSinGuiones = item.slice(2);
+      respuesta[propiedadSinGuiones] = argv[ind + 1];
+      }
+  });
+  return respuesta;
 }
 
-function main() {
-  const comandosAEjecutar = parsearARGV(process.argv);
 
-  pelis.searchByCriteria(comandosAEjecutar);
+function main() {
+
+  const comandosAEjecutar = parsearARGV(process.argv.slice(2));
+  return console.table(pelis.searchByCriteria(comandosAEjecutar));
 }
 
 main();
