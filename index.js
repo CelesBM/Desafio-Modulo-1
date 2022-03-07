477a3597b944764b22974632ad631cd8b851fa98
CelesBM <90277599+CelesBM@users.noreply.github.com>
2022-03-07 18:03:55 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const pelis = require("./pelis");
 
 function parsearARGV(argv) {
-  
   const respuesta = {};
   
   argv.forEach(function (item, ind) {
@@ -9,17 +8,13 @@ function parsearARGV(argv) {
       const propiedadSinGuiones = item.slice(2);
       respuesta[propiedadSinGuiones] = argv[ind + 1];
       }
-  });
-  
+  }); 
   return respuesta;
 }
 
-
 function main() {
-
   const comandosAEjecutar = parsearARGV(process.argv.slice(2));
   return console.table(pelis.searchByCriteria(comandosAEjecutar)); 
-
 }
 
 main();
